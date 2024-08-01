f33f4a9a53c73e327568bba213045e31ed2ea40d
alexarlord-boop <rezerw0.1@yandex.ru>
2024-08-01 15:37:18 +0300

    MUSE-2024 Added margin

--- album-catalog/src/components/icons/AssetIsAbsent.jsx
+++ album-catalog/src/components/icons/AssetIsAbsent.jsx
@@ -2,7 +2,7 @@ import {TbWorldQuestion} from "react-icons/tb";
 import React from "react";
 
 export const AssetIsAbsent = ({
-                                  size = 128, className = "mx-auto my-auto ", ...props
+                                  size = 128, className = "mx-auto my-auto my-10", ...props
                               }) => (
     <TbWorldQuestion size={size} className={className} {...props}/>
 );
